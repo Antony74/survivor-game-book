d3cde3a99f8863c694301d2071b4082e7b0d0aa8
Antony74 <akb@akb.me.uk>
2022-06-14 22:42:52 +0100

    Good, that seems to be working

--- extract.ts
+++ extract.ts
@@ -2,8 +2,16 @@ import fs from 'fs/promises';
 import path from 'path';
 const PDF2json = require('pdf2json');
 
-const main = async () => {
+interface RObject {
+    T: string;
+}
 
+interface TextObject {
+    R: RObject[];
+    y: number;
+}
+
+const main = async () => {
     const outputDir = path.join(__dirname, 'pages');
 
     try {
@@ -16,14 +24,25 @@ const main = async () => {
     const pdfParser = new PDF2json();
 
     pdfParser.on('pdfParser_dataReady', (pdfData: any) => {
-        pdfData.Pages.forEach((page: any, index: number) => {
-            const pageNumber = index + 1;
-            const filename = `page${pageNumber}.html`;
+        pdfData.Pages.forEach((page: any, pageIndex: number) => {
+            const pageNumber = pageIndex + 1;
+            const filename = `page${pageNumber}.txt`;
             console.log(`Page ${pageNumber}`);
 
-            const texts = page.Texts.map((text: any) => text.R.map((r: any) => decodeURIComponent(r.T)).join('\n'));
+            let prevY = 0;
+
+            const texts = page.Texts.map((textObject: TextObject, textIndex: number) => {
+                let text = textObject.R.map((r: RObject) => decodeURIComponent(r.T)).join(' ');
+
+                if (textIndex && textObject.y > prevY) {
+                    text = '\n' + text;
+                }
+
+                prevY = textObject.y;
+                return text;
+            });
             texts[0] += '\n';
-            fs.writeFile(path.join(__dirname, 'pages', filename), texts.join('\n'));
+            fs.writeFile(path.join(__dirname, 'pages', filename), texts.join(' '));
         });
     });
 
