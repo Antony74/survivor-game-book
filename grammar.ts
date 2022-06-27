f0be346d1c4c8ce574db1c7f5a840ef4713a5968
Antony74 <akb@akb.me.uk>
2022-06-27 21:25:51 +0100

    More grammar

--- grammar.ts
+++ grammar.ts
@@ -1,29 +1,66 @@
 import fs from 'fs/promises';
 import fetch from 'node-fetch';
 
+interface Replacement {
+    value: string;
+}
+
+interface Rule {
+    id: string;
+}
+
+interface Match {
+    replacements: Replacement[];
+    offset: number;
+    length: number;
+    rule: Rule;
+}
+
+interface Grammar {
+    matches: Match[];
+}
+
 const main = async () => {
     const filenames = await fs.readdir('pages');
 
     for (const filename of filenames) {
-        const text = await fs.readFile(`pages/${filename}`, { encoding: 'utf8' });
+        if (filename.split('.').length === 2) {
+            let text = await fs.readFile(`pages/${filename}`, { encoding: 'utf8' });
 
-       const params = new URLSearchParams({text, language: 'en-GB', enabledOnly: 'false'})
+            // Get rid of any capitalisations of the word 'You', trusting the
+            // UPPERCASE_SENTENCE_START rule to restore any needed.
+            text = text.replaceAll('You', 'you');
 
-        const response = await fetch('http://localhost:8081/v2/check', {
-            method: 'POST',
-            body: params.toString(),
-        });
+            const params = new URLSearchParams({ text, language: 'en-GB', enabledOnly: 'false' });
 
-        if (response.status !== 200) {
-            console.log(response.status)
-            console.error(await response.text());
-            break;
+            const response = await fetch('http://localhost:8081/v2/check', {
+                method: 'POST',
+                body: params.toString(),
+            });
+
+            if (response.status !== 200) {
+                console.log(response.status);
+                console.error(await response.text());
+                break;
+            }
+
+            const grammar: Grammar = await response.json();
+            grammar.matches.reverse();
+
+            grammar.matches.forEach(match => {
+                if (match.replacements.length === 1) {
+                    if (match.rule.id === 'UPPERCASE_SENTENCE_START') {
+                        text =
+                            text.slice(0, match.offset) +
+                            match.replacements[0].value +
+                            text.slice(match.offset + match.length);
+                    }
+                }
+            });
+
+            fs.writeFile(`pages/${filename}`, text);
+            fs.writeFile(`pages/${filename}.json`, JSON.stringify(grammar, null, 2));
         }
-
-        const json = await response.json();
-
-        console.log(JSON.stringify(json, null, 2));
-        break;
     }
 };
 
